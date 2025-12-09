22a0f34ec06121604d1a2a6d95606b287138cde1
1579923417 <1579923417@qq.com>
2025-12-09 14:52:32 +0800

    feat(ruoyi-app): 新增ruoyi-app模块及相关基础配置
    添加ruoyi-app模块到父pom.xml
    初始化ruoyi-app模块，包含：
    - Spring Boot应用主类
    - MyBatis配置
    - 国际化配置
    - 日志配置
    - 数据库连接配置
    - 应用基础配置
    - 测试类

---
+++ ruoyi-app/src/main/java/org/jeecg/ruoyiapp/RuoyiAppApplication.java
@@ -0,0 +1,24 @@
+package org.jeecg.ruoyiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiAppApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  APP--启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+
+}
---
+++ ruoyi-app/src/test/java/org/jeecg/ruoyiapp/RuoyiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeecg.ruoyiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
